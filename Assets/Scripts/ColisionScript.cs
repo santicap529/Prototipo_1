cd3c0eb6b326a63e2703d0912ec0c1d75aeddbb0
santicap529 <santinocapelli2010@gmail.com>
2026-05-21 10:36:54 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

---
+++ Assets/Scripts/ColisionScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Colectable"))
+       {
+         Destroy(other.gameObject);
+       }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
